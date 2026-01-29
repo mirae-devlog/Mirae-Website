a940abf79d79a3c75debc92f2e7eb42da32b1022
mirae-devlog <153251950+MiraeNK@users.noreply.github.com>
2026-01-30 04:01:35 +0700

    Reset config for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  
-  // PENTING: Sesuaikan dengan nama repository GitHub Anda persis!
-  basePath: '/Mirae-Website',
-  
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     unoptimized: true,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
